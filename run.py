ae8ef49de7db3aa34fae9740eddf71ee1895f05f
rl33-glitch <rosslowe30@gmail.com>
2022-05-10 11:14:18 +0000

    add while loop and player input

--- run.py
+++ run.py
@@ -10,3 +10,10 @@ secret_word = random.choice(words)
 guessed_so_far = len(secret_word) * "_ "
 
 
+while True:
+    print(guessed_so_far)
+
+    guess = input("I'm thinking of a word, can you guess a letter?: ")
+
+    if guess in secret_word:
+        print("You guessed correctly")
