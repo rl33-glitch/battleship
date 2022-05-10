e685278b77aa836a0f6760a3841d88a4d9ee7caa
rl33-glitch <rosslowe30@gmail.com>
2022-05-10 10:34:55 +0000

    3 variables greated and random imported

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+import random
+
+words = ["cat", "four", "spots"]
+secret_word = random.choice(words)
+
+guessed_so_far = len(secret_word) * "_ "
+
+
