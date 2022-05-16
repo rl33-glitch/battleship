2f09f6b6bf21f54c0bac4b2e38490495a526d6c8
rl33-glitch <rosslowe30@gmail.com>
2022-05-16 12:41:36 +0000

    add letter printed to user if correct functionality

--- run.py
+++ run.py
@@ -5,23 +5,10 @@
 
 import random
 
-def animation(incorrect,board):
-    if incorrect == 0:
-        print(board)
-    
-    elif incorrect == 1:
-        print ("      _________")
-        print ("     |/")
-        print ("     |")
-        print ("     |")
-        print ("     |")
-        print ("    _|_")
-
 words = ["cat", "four", "spots"]
 secret_word = random.choice(words)
 
 guessed_so_far = len(secret_word) * "_ "
-
 
 while True:
     print(guessed_so_far)
@@ -29,4 +16,13 @@ while True:
     guess = input("I'm thinking of a word, can you guess a letter?: ")
 
     if guess in secret_word:
-        print("You guessed correctly")
+        for position, letter in enumerate(secret_word):
+            if guess == letter:
+                guessed_so_far = list(guessed_so_far)
+                guessed_so_far[position] = letter
+                guessed_so_far = ''.join(guessed_so_far)
+
+    if guessed_so_far == secret_word:
+        break
+    
+    print("game over")
