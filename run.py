35da76d0ed44f97259da60d65bd6a1f1413a91e6
rl33-glitch <rosslowe30@gmail.com>
2022-05-19 12:33:01 +0000

    restarting project

--- run.py
+++ run.py
@@ -3,26 +3,3 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
 
-import random
-
-words = ["cat", "four", "spots"]
-secret_word = random.choice(words)
-
-guessed_so_far = len(secret_word) * "_ "
-
-while True:
-    print(guessed_so_far)
-
-    guess = input("I'm thinking of a word, can you guess a letter?: ")
-
-    if guess in secret_word:
-        for position, letter in enumerate(secret_word):
-            if guess == letter:
-                guessed_so_far = list(guessed_so_far)
-                guessed_so_far[position] = letter
-                guessed_so_far = ''.join(guessed_so_far)
-
-    if guessed_so_far == secret_word:
-        break
-    
-    print("game over")
