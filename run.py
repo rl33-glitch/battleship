13819d916dcaec2ba39f57f4bf739f210f4c25e4
rl33-glitch <rosslowe30@gmail.com>
2022-05-19 12:36:59 +0000

    add import random and words variable with guess words variable

--- run.py
+++ run.py
@@ -2,4 +2,7 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
+import random
 
+words=["car","roadrunner", "dinosaur"]
+guess_words=random.choice(words)
