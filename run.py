15b2fc9577b75ec70c2962d24b6353b30fec94e4
rl33-glitch <rosslowe30@gmail.com>
2022-05-21 08:37:15 +0000

    fixed guess word variable

--- run.py
+++ run.py
@@ -4,68 +4,29 @@
 
 import random
 
-words = ["car", "roadrunner", "dinosaur"]
-guess_words = random.choice(words)
 
-c=guess_word[:] 
-indexes = [] 
-guess_word = list(guess_word)
-correct_ans = guess_word
+words=["car","roadrunner","dinosaur"]
+guess_word=random.choice(words)
 
-def hint_gen(guess_word,c):
-    for i in range(0,len(guess_word),3): 
 
-        c=c.replace(c[i],"_")
-    return c
-    
-c=hint_gen(guess_word,c) 
 
-def detector(guess_word,guess):
-    a=[]
-    guess_word = list(guess_word)
-    #print(r)
-    count=0
-    for i in guess_word:
-        if i==guess:
-            a.append(count)
-        count+=1
-    return a 
 
-def modifier(c,guess_word="roadrunner",guess="r"): 
-    a = detector(guess_word,guess) 
-    c = list(c) 
-    r ="" 
-    print(c,a) 
-    for i in a:
-        c[i]=guess
-        guess_word[i]="_" 
-    for j in c: 
-        r = r+j
-    return r 
+c=guess_word[:] # will make a copy of the selected guessword
+correct_ans=guess_word
 
-#name=input("Enter your name: ")
-#print(f"Hello {name}, Welcome to Hangman!") 
-#print(list(guess_word))
-tries = 7
-while tries>0 and c!=correct_ans:
-    print("Try to guess this word", c)
-    guess = input("Guess a letter: ")
-    for i in guess_word:
-        if guess == i:
-            print("You guessed ",guess,"correctly")
-            c=modifier(c,guess_word,guess)
-            print(c)
+indexes=[] # for use in detector function
+guess_word=list(guess_word)
 
 pics= ['''
-     +---+
-         |
-         |
-         |
-        ===''', '''
     +---+
-    O    |
-         |
-         |
+        |
+        |
+        |
+       ===''', '''
+    +---+
+    O   |
+        |
+        |
        ===''', '''
     +---+
     O   |
@@ -93,4 +54,3 @@ pics= ['''
    / \  |
       ===''']
 
-print(pics[6])
